e44e1f564976c6267088daf61790d104b0a47485
Augusto Marques <anes.2017@alunos.utfpr.edu.br>
2022-08-19 23:46:33 -0300

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="home">
+Precisa de ajuda com os preparos do seu evento?
+</div>
+</template>
+
+<style scoped>
+.home{
+  background-image: url('../../assets/food_background_generic.jpg')
+}
+</style>
